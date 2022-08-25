534adcd07335dc0d4bda7df57ac2dfcd4b970ca5
lorobert <lorobert@student.42lausanne.ch>
2022-08-25 10:47:17 +0200

    strlen corr

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lorobert <lorobert@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 16:27:25 by lorobert          #+#    #+#             */
-/*   Updated: 2022/08/23 10:33:15 by lorobert         ###   ########.fr       */
+/*   Updated: 2022/08/25 10:47:04 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ size_t	ft_strlen(char const *s)
 {
 	size_t	i;
 
-	if (!s)
-		return (0);
 	i = 0;
 	while (s[i])
 		i++;
