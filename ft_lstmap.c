a7cff6bba3e416af62126204f3815eb6cd5eb207
lorobert <lorobert@student.42lausanne.ch>
2022-08-25 15:28:02 +0200

    finished

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lorobert <lorobert@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 10:35:40 by lorobert          #+#    #+#             */
-/*   Updated: 2022/08/25 10:37:29 by lorobert         ###   ########.fr       */
+/*   Updated: 2022/08/25 15:19:29 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,26 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	(void)lst;
-	(void)f;
-	(void)del;
-	return (NULL);
+	t_list	*res;
+	t_list	*start;
+
+	if (!lst)
+		return (NULL);
+	res = ft_lstnew(f(lst->content));
+	if (!res)
+		return (NULL);
+	start = res;
+	lst = lst->next;
+	while (lst)
+	{
+		res->next = ft_lstnew(f(lst->content));
+		if (!res->next)
+		{
+			ft_lstclear(&start, del);
+			return (NULL);
+		}
+		res = res->next;
+		lst = lst->next;
+	}
+	return (start);
 }
