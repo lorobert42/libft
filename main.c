3d48b4099b60d6d0a4ce9081c96e94753f4264c6
Loic Robert <lorobert@c1r7s4.42lausanne.ch>
2022-10-11 11:03:39 +0200

    tests

--- main.c
+++ main.c
@@ -6,9 +6,13 @@
 /*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 10:17:20 by lorobert          #+#    #+#             */
-/*   Updated: 2022/10/11 10:18:02 by lorobert         ###   ########.fr       */
+/*   Updated: 2022/10/11 10:47:09 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
 
 #include <stdio.h>
 #include <ctype.h>
@@ -19,4 +23,10 @@
 
 int	main()
 {
+	// isalpha
+	printf("Testing isalpha\n");
+	if (ft_isalpha('A') == isalpha('A'))
+		printf(GREEN "A is alpha" RESET "\n");
+	else
+		fprintf(stderr, RED "A is not alpha" RESET "\n");
 }
